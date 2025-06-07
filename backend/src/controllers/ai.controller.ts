9bb264ceb328c5515f7782b78ecadbd7aee616a9
rickid0 <luruicheng@me.com>
2025-06-07 22:21:07 +0800

    feat(ai): select model

--- backend/src/controllers/ai.controller.ts
+++ backend/src/controllers/ai.controller.ts
@@ -4,12 +4,19 @@ import deepseek from '../models/deepseek.model'
 import AIService from '../services/ai.service'
 import { ApiError } from '../utils/ApiError'
 export const evaluateCode = async (req: Request, res: Response) => {
-  const { title, code } = req.body
+  const { title, code, model } = req.body
   try {
-    const aiRes = await AIService.evaluateCode(title, code)
-    res.status(200).json({
-      message: aiRes,
-    })
+    if (model) {
+      const aiRes = await AIService.evaluateCode(title, code, model)
+      res.status(200).json({
+        message: aiRes,
+      })
+    } else {
+      const aiRes = await AIService.evaluateCode(title, code)
+      res.status(200).json({
+        message: aiRes,
+      })
+    }
   } catch (error) {
     if (error instanceof ApiError) {
       console.log(error)
@@ -19,12 +26,19 @@ export const evaluateCode = async (req: Request, res: Response) => {
 }
 
 export const getAnswer = async (req: Request, res: Response) => {
-  const { title, content, lang } = req.body
+  const { title, content, lang, model } = req.body
   try {
-    const aiRes = await AIService.getAnswer(title, content, lang)
-    res.status(200).json({
-      message: aiRes,
-    })
+    if (model) {
+      const aiRes = await AIService.getAnswer(title, content, lang)
+      res.status(200).json({
+        message: aiRes,
+      })
+    } else {
+      const aiRes = await AIService.getAnswer(title, content, lang, model)
+      res.status(200).json({
+        message: aiRes,
+      })
+    }
   } catch (error) {
     if (error instanceof ApiError) {
       console.log(error)
@@ -34,12 +48,19 @@ export const getAnswer = async (req: Request, res: Response) => {
 }
 
 export const getAnalyzation = async (req: Request, res: Response) => {
-  const { title, content } = req.body
+  const { title, content, model } = req.body
   try {
-    const aiRes = await AIService.analyzeProblem(title, content)
-    res.status(200).json({
-      message: aiRes,
-    })
+    if (model) {
+      const aiRes = await AIService.analyzeProblem(title, content, model)
+      res.status(200).json({
+        message: aiRes,
+      })
+    } else {
+      const aiRes = await AIService.analyzeProblem(title, content)
+      res.status(200).json({
+        message: aiRes,
+      })
+    }
   } catch (error) {
     if (error instanceof ApiError) {
       console.log(error)
