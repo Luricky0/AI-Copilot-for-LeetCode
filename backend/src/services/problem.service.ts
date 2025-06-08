19f451c141960ff0118a896f5cc9be8997b194ec
rickid0 <luruicheng@me.com>
2025-06-08 20:49:14 +0800

    refactor: problem service

---
+++ backend/src/services/problem.service.ts
@@ -0,0 +1,62 @@
+import Problem from '../models/problem.model'
+import { Iuser } from '../models/user.model'
+import { ApiError } from '../utils/ApiError'
+
+const escapeRegex = (text: string): string => {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
+}
+
+const getPage = async (
+  page: number,
+  limit: number,
+  skip: number,
+  searchQuery: string,
+  difficultyFilter: string,
+  likedOnly: boolean,
+  completedOnly: boolean,
+  user: Iuser
+) => {
+  const query: any = {}
+
+  if (searchQuery && typeof searchQuery === 'string') {
+    const fuzzy = searchQuery
+      .split('')
+      .map((c) => escapeRegex(c))
+      .join('.*')
+    query.title = { $regex: fuzzy, $options: 'i' }
+  }
+
+  if (difficultyFilter && difficultyFilter !== 'All') {
+    query.difficulty = difficultyFilter
+  }
+
+  if (likedOnly && user) {
+    const likedIds = user?.likedProblemsIDs.map((p) => p.problemId)
+    query._id = { $in: likedIds }
+  } else {
+    throw new ApiError(400, 'No user')
+  }
+
+  if (completedOnly && user) {
+    const completedIds = user?.completedProblemsIDs.map((p) => p.problemId)
+    query._id = { $in: completedIds }
+  } else {
+    throw new ApiError(400, 'No user')
+  }
+
+  const [problems, totalproblems] = await Promise.all([
+    Problem.find(query).skip(skip).limit(limit),
+    Problem.countDocuments(query),
+  ])
+  return {
+    page,
+    totalPages: Math.ceil(totalproblems / limit),
+    totalproblems,
+    problems,
+  }
+}
+
+const ProblemService = {
+  getPage,
+}
+export default ProblemService
