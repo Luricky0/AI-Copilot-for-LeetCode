bcf2f0f6b8fdcf2f54169264582c62eeb0edf0e7
rickid0 <luruicheng@me.com>
2025-06-09 23:14:52 +0800

    refactor: problem controller

--- backend/src/controllers/problem.controller.ts
+++ backend/src/controllers/problem.controller.ts
@@ -3,74 +3,37 @@ import Problem from '../models/problem.model'
 import { ObjectId } from 'mongodb'
 import jwt from 'jsonwebtoken'
 import User from '../models/user.model'
+import { UserService } from '../services/user.service'
+import ProblemService from '../services/problem.service'
 
 const escapeRegex = (text: string): string => {
   return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
 }
 
 export const getPaginatedProblems = async (req: Request, res: Response) => {
+  const {
+    page,
+    limit,
+    skip,
+    searchQuery,
+    difficultyFilter,
+    likedOnly,
+    completedOnly,
+  } = req.body
   try {
-    const page = parseInt(req.query.page as string) || 1
-    const limit = parseInt(req.query.limit as string) || 20
-    const skip = (page - 1) * limit
-    const searchQuery = req.query.search
-    const difficultyFilter = req.query.difficulty
-    const likedOnly = req.query.likedOnly === 'true'
-    const completedOnly = req.query.completedOnly === 'true'
-
-    const query: any = {}
-
-    if (searchQuery && typeof searchQuery === 'string') {
-      const fuzzy = searchQuery
-        .split('')
-        .map((c) => escapeRegex(c))
-        .join('.*')
-      query.title = { $regex: fuzzy, $options: 'i' }
-    }
-
-    if (difficultyFilter && difficultyFilter !== 'All') {
-      query.difficulty = difficultyFilter
-    }
-
-    if (likedOnly) {
-      const token = req.headers.authorization?.split(' ')[1]
-      if (token) {
-        const decoded: any = jwt.verify(token, process.env.JWT_SECRET!)
-        const userId = decoded.id
-        const user = await User.findOne({ id: userId })
-        const likedIds = user?.likedProblemsIDs.map((p) => p.problemId)
-        query._id = { $in: likedIds }
-      } else {
-        res.status(400).json({
-          message: 'Bad token',
-        })
-      }
-    } else if (completedOnly) {
-      const token = req.headers.authorization?.split(' ')[1]
-      if (token) {
-        const decoded: any = jwt.verify(token, process.env.JWT_SECRET!)
-        const userId = decoded.id
-        const user = await User.findOne({ id: userId })
-        const completedIds = user?.completedProblemsIDs.map((p) => p.problemId)
-        query._id = { $in: completedIds }
-      } else {
-        res.status(400).json({
-          message: 'Bad token',
-        })
-      }
-    }
-
-    const [problems, totalproblems] = await Promise.all([
-      Problem.find(query).skip(skip).limit(limit),
-      Problem.countDocuments(query),
-    ])
-
-    res.status(200).json({
+    const user = await UserService.getUserByToken(req)
+    const getPageRes = await ProblemService.getPage(
       page,
-      totalPages: Math.ceil(totalproblems / limit),
-      totalproblems,
-      problems,
-    })
+      limit,
+      skip,
+      searchQuery,
+      difficultyFilter,
+      likedOnly,
+      completedOnly,
+      user!
+    )
+
+    res.status(200).json(getPageRes)
   } catch (error) {
     res.status(500).json({ message: 'Failed', error })
   }
