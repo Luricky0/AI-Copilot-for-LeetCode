fffc997a0dbd20f0f67f2d5a06c1aca38ba685fc
rickid0 <luruicheng@me.com>
2025-09-17 19:18:37 -0700

    refactor(ai service)

--- api-gateway/src/routes/ai.ts
+++ api-gateway/src/routes/ai.ts
@@ -4,6 +4,8 @@ import axios from 'axios'
 
 const router = express.Router()
 const AI_SERVICE_URL = process.env.AI_SERVICE_URL
+const USER_SERVICE_URL = process.env.USER_SERVICE_URL
+
 const transferToAIService = async (
   req: Request,
   res: Response,
@@ -21,15 +23,23 @@ const transferToAIService = async (
     res.status(err.response?.status || 500).json({ error: err.message })
   }
 }
-const evaluate = (req: Request, res: Response) => {
+const evaluate = async (req: Request, res: Response) => {
   try {
-    const subRes = await axios.post(`${USER_SERVICE_URL}/addOneSubmission`)
+    const subRes = await axios.post(
+      `${USER_SERVICE_URL}/addOneSubmission`,
+      req.body
+    )
+    const embeddingRes = await axios.get(`${USER_SERVICE_URL}/embedding`)
+    const embedding = embeddingRes.data
+    const payload = embedding ? { ...req.body, embedding } : req.body
+    const evalRes = await axios.post(`${AI_SERVICE_URL}/evaluate`, payload)
+    res.status(200).json(evalRes.data)
   } catch (err: any) {
     console.log(err)
     res.status(err.response?.status || 500).json({ error: err.message })
   }
 }
-router.post('/evaluate')
+router.post('/evaluate', evaluate)
 router.post('/answer', (req, res) => transferToAIService(req, res, 'answer'))
 router.post('/analyze', (req, res) => transferToAIService(req, res, 'analyze'))
 router.get('/request', (req, res) =>
